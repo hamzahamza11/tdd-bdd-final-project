8067348bbbf2a4985bbffbd1a29bba3b7c2ccb96
hamzahamza11 <sabirha00@gmail.com>
2024-02-20 19:48:14 +0100

    cmplete assignment

--- tests/factories.py
+++ tests/factories.py
@@ -1,34 +1,50 @@
-# Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# pylint: disable=too-few-public-methods
-
-"""
-Test Factory to make fake objects for testing
-"""
 import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
-from service.models import Product, Category
-
+from .models import Product, Category  # Adjust the import path according to your project structure
 
 class ProductFactory(factory.Factory):
-    """Creates fake products for testing"""
-
+    """Creates fake products for testing."""
     class Meta:
-        """Maps factory to data model"""
-
+        """Maps factory to the Product data model."""
         model = Product
 
+    # Sequential ID for each product instance
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+
+    # Random product name from a predefined list of choices
+    name = FuzzyChoice(
+        choices=[
+            "Hat",
+            "Pants",
+            "Shirt",
+            "Apple",
+            "Banana",
+            "Pots",
+            "Towels",
+            "Ford",
+            "Chevy",
+            "Hammer",
+            "Wrench"
+        ]
+    )
+
+    # Fake text for product description
+    description = factory.Faker("text")
+
+    # Random price within the specified range, with two decimal places
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+
+    # Randomly available or not
+    available = FuzzyChoice(choices=[True, False])
+
+    # Random category from a predefined list of choices
+    category = FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ]
+    )
